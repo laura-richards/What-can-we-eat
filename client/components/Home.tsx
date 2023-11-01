286920daaebb3fe04a3a8d28af1a97f91147cd98
Laura Richards <lionheartnz85@gmail.com>
2023-11-01 22:59:28 +1300

    create components

---
+++ client/components/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section>
+      <h2 className="page-title">Ideas to come</h2>
+    </section>
+  )
+}
+
+// export default Home()
---
+++ client/components/Nav.tsx
@@ -0,0 +1,13 @@
+function Nav() {
+  return (
+    <nav
+      className="nav-container"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      nav to come here
+    </nav>
+  )
+}
+
+export default Nav
