3d58f9ee00dc43044ad6c053a6026369060afc22
Laura Richards <lionheartnz85@gmail.com>
2024-03-29 21:54:42 +1300

    update seeds and migrations

--- server/db/migrations/20240229022017_users.js
+++ server/db/migrations/20240229022017_users.js
@@ -6,6 +6,7 @@ export async function up(knex) {
   await knex.schema.createTable('users', (table) => {
     table.integer('id').primary()
     table.string('name')
+    table.string('username')
     table.string('email')
     table.string('auth0Id')
   })
--- server/db/seeds/1-users.js
+++ server/db/seeds/1-users.js
@@ -4,8 +4,23 @@
  */
 export async function seed(knex) {
   await knex('users').insert([
-    { name: 'Jane Doe', email: 'string1@gmail.com', auth0Id: 'auth101' },
-    { name: 'John Doe', email: 'string2@gmail.com', auth0Id: 'auth102' },
-    { name: 'Jake Doe', email: 'string3@gmail.com', auth0Id: 'auth103' },
+    {
+      name: 'Jane Doe',
+      email: 'string1@gmail.com',
+      username: 'one1',
+      auth0Id: 'auth101',
+    },
+    {
+      name: 'John Doe',
+      email: 'string2@gmail.com',
+      username: 'one2',
+      auth0Id: 'auth102',
+    },
+    {
+      name: 'Jake Doe',
+      email: 'string3@gmail.com',
+      username: 'one3',
+      auth0Id: 'auth103',
+    },
   ])
 }
