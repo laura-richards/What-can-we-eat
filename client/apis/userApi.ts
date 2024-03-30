77f34977833175a5081f84a4fc8307c93efe32ea
Laura Richards <lionheartnz85@gmail.com>
2024-03-30 20:55:59 +1300

    remove console.log in api

--- client/apis/userApi.ts
+++ client/apis/userApi.ts
@@ -4,7 +4,6 @@ import { NewUserDetails } from '../../models/userModels'
 const rootURL = 'api/v1/user'
 
 export async function addNewUser(newUser: NewUserDetails) {
-  console.log('tried to add:', newUser)
   const res = await request.post(rootURL).send(newUser)
   if (res.status !== 200) {
     throw new Error('failed to add user')
